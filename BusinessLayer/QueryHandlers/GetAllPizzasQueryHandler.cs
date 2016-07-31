65a5114ba3978fb2ca3577beffcc3033f4e5770b
Adarsh Lal <adarshlal@hotmail.com>
2016-07-31 16:20:31 +1200

    Minor changes

--- BusinessLayer/QueryHandlers/GetAllPizzasQueryHandler.cs
+++ BusinessLayer/QueryHandlers/GetAllPizzasQueryHandler.cs
@@ -34,6 +34,8 @@
                                             new Pizza {id=1, Name = "Chastity", Description="Pure pizza with only the best ingriedents to love.", Price=10D },
                                             new Pizza {id=1, Name = "Temperance", Description="Vege lovers delight.", Price=9D },
                                             new Pizza {id=1, Name = "Charity", Description="Meat Lovers are going to be oncloud 9.", Price=15D },
+                                            new Pizza {id=1, Name = "Curiousity", Description="Dice with the devil for a random chance pizza.", Price=15D },
+                                            new Pizza {id=1, Name = "Devotion", Description="Chicken, garlic and secret sauce made from the foountain of youth.", Price=15D },
 
                                         };
 
