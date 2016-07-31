a56255974b66202be0049a7bdc7480505a727414
Adarsh Lal <adarshlal@hotmail.com>
2016-07-31 12:30:06 +1200

    Add missing files

---
+++ HeavenlyPizzeriaWebUI/Controllers/OrderPizzaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeavenlyPizzeriaWebUI.Controllers
+{
+    public class OrderPizzaController : Controller
+    {
+        // GET: OrderPizza
+        public ActionResult Index(int id, HeavenlyPizzeriaWebUI.Models.Pizza pizza)
+        {
+            var model = new HeavenlyPizzeriaWebUI.Models.PizzaOrder { Pizza = pizza };
+            return View(model);
+        }
+    }
+}
