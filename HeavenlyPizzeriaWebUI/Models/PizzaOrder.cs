89d8a4af2c736330880d86fa53f3809e80c44045
Adarsh Lal <adarshlal@hotmail.com>
2016-07-31 21:49:33 +1200

    validation

--- HeavenlyPizzeriaWebUI/Models/PizzaOrder.cs
+++ HeavenlyPizzeriaWebUI/Models/PizzaOrder.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+
 
 namespace HeavenlyPizzeriaWebUI.Models
 {
@@ -9,8 +11,11 @@ namespace HeavenlyPizzeriaWebUI.Models
     {
         public Pizza Pizza { get; set; }
 
+        [Required(ErrorMessage ="Thine name is a must")]
         public String Name { get; set; }
+        [Required(ErrorMessage = "We ought to know where to go")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "You have a number in mind?")]
         public int Quantity { get; set; }
         public string Instructions { get; set; }
     }
