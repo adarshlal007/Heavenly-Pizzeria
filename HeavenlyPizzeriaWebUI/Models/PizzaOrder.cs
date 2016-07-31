f5b66bc781c43615e1355ed9abc697f4b965b1b6
Adarsh Lal <adarshlal@hotmail.com>
2016-07-31 12:28:22 +1200

    Fixes to DI and new view

---
+++ HeavenlyPizzeriaWebUI/Models/PizzaOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeavenlyPizzeriaWebUI.Models
+{
+    public class PizzaOrder
+    {
+        public Pizza Pizza { get; set; }
+
+        public String Name { get; set; }
+        public string Address { get; set; }
+        public int Quantity { get; set; }
+        public string Instructions { get; set; }
+    }
+}
