690202334edf2a862844b72d61e79cc2bb60bcef
Adarsh Lal <adarshlal@hotmail.com>
2016-07-31 19:30:27 +1200

    Added repo for pizza

--- BusinessLayer/QueryHandlers/GetAllPizzasQueryHandler.cs
+++ BusinessLayer/QueryHandlers/GetAllPizzasQueryHandler.cs
@@ -30,14 +30,15 @@
         private Pizza[] GetAllPizzas()
         {
             //This would be call to DAL layer (entity framework or massive micro ORM)
-            var pizzas = new List<Pizza> {
-                                            new Pizza {id=1, Name = "Chastity", Description="Pure pizza with only the best ingriedents to love.", Price=10D },
-                                            new Pizza {id=1, Name = "Temperance", Description="Vege lovers delight.", Price=9D },
-                                            new Pizza {id=1, Name = "Charity", Description="Meat Lovers are going to be oncloud 9.", Price=15D },
-                                            new Pizza {id=1, Name = "Curiousity", Description="Dice with the devil for a random chance pizza.", Price=15D },
-                                            new Pizza {id=1, Name = "Devotion", Description="Chicken, garlic and secret sauce made from the foountain of youth.", Price=15D },
+            var pizzas = Repo.DbContext.Pizzas();
+            //var pizzas = new List<Pizza> {
+            //                                new Pizza {id=1, Name = "Chastity", Description="Pure pizza with only the best ingriedents to love.", Price=10D },
+            //                                new Pizza {id=1, Name = "Temperance", Description="Vege lovers delight.", Price=9D },
+            //                                new Pizza {id=1, Name = "Charity", Description="Meat Lovers are going to be oncloud 9.", Price=15D },
+            //                                new Pizza {id=1, Name = "Curiousity", Description="Dice with the devil for a random chance pizza.", Price=15D },
+            //                                new Pizza {id=1, Name = "Devotion", Description="Chicken, garlic and secret sauce made from the foountain of youth.", Price=15D },
 
-                                        };
+            //                            };
 
             return pizzas.ToArray();
         }
---
+++ Repo/DbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Contract.DTOs;
+
+namespace Repo
+{
+    public class DbContext
+    {
+        public static Pizza[] Pizzas()
+        {
+            //This would be call to DAL layer (entity framework or massive micro ORM)
+            var pizzas = new List<Pizza> {
+                                            new Pizza {id=1, Name = "Chastity", Description="Pure pizza with only the best ingriedents to love.", Price=10D },
+                                            new Pizza {id=1, Name = "Temperance", Description="Vege lovers delight.", Price=9D },
+                                            new Pizza {id=1, Name = "Charity", Description="Meat Lovers are going to be oncloud 9.", Price=15D },
+                                            new Pizza {id=1, Name = "Curiousity", Description="Dice with the devil for a random chance pizza.", Price=15D },
+                                            new Pizza {id=1, Name = "Devotion", Description="Chicken, garlic and secret sauce made from the foountain of youth.", Price=15D },
+
+                                        };
+
+            return pizzas.ToArray();
+        }
+    }
+}
